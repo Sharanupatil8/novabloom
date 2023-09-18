2977a5dbdce57bd6053d988aad6e35fa268c6bdb
Sharanu P <93566630+Sharanupatil8@users.noreply.github.com>
2023-09-18 21:59:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//typed js intialization
+
+var typed = new Typed("#element", {
+  strings: ["Web Design.", " Digital Marketing."],
+  typeSpeed: 150,
+  backDelay: 1000,
+  fadeOut: false,
+  fadeOutClass: "typed-fade-out",
+  fadeOutDelay: 500,
+  loop: true,
+  loopCount: Infinity,
+});
